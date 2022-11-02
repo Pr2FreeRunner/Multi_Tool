d365fc79545e111fe2abebdbb23d96c393c6ca2b
Tobias <Tobia@DESKTOP-K02VCHB>
2022-11-02 06:53:18 +0100

    Build Version

--- DataAccess/Accessors/VersionFetcher.cs
+++ DataAccess/Accessors/VersionFetcher.cs
@@ -30,25 +30,14 @@ namespace DataAccess.Accessors
 
         private string GetBuildVersion(JObject json)
         {
-            // the name of the build version inside the JSON file is unknown
-            // as it has not been implemented yet
-            // so i am just dummy trying some values and hope it will work once Bls199 update the API
-            // so I dont have to make a new release of the tool later
-
             if (json == null)
                 return string.Empty;
 
-            var names = new string[] { "build_version", "buildversion", "buildVersion", "build", "BuildVersion",
-                                       "client_version", "clientversion", "clientVersion", "client", "ClientVersion",
-                                       "code_version", "codeversion", "codeVersion", "CodeVersion"};
+            var name  = "build";
+            var value = json?.GetValue(name)?.Value<string>() ?? string.Empty;
 
-            foreach (var name in names)
-            {
-                var value = json?.GetValue(name)?.Value<string>() ?? string.Empty;
-
-                if (!string.IsNullOrWhiteSpace(value))
-                    return value;
-            }
+            if (!string.IsNullOrWhiteSpace(value))
+                return value;
 
             return string.Empty;
 
